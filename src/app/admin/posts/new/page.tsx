73144a52c2b609970fd38eb5f7541c352c613d60
Ponkel33 <the.orangutans.w@gmail.com>
2025-10-13 21:09:35 +0900

    PostFormのコンポーネント化

--- src/app/admin/posts/new/page.tsx
+++ src/app/admin/posts/new/page.tsx
@@ -1,87 +1,10 @@
 "use client"
-
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { Category } from "@/app/_types/Category";
-import { CategoriesSelect } from "../_components/CategoriesSelect";
+import PostForm from "../_components/PostForm"
 
 export default function AdminPostsId() {
-  const [title, setTitle] = useState('')
-  const [content, setContent] = useState('')
-  const [thumbnailUrl, setThumbnailUrl] = useState('https://placehold.jp/800x400.png')
-  const [categories, setCategories] = useState<Category[]>([])
-
-  const router = useRouter()
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    
-    const res = await fetch("/api/admin/posts", {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        title,
-        content,
-        thumbnailUrl,
-        categories,
-      }),
-    })
-
-    const { id } = await res.json()
-
-    router.push(`/posts/${id}`)
-
-    alert('登録しました')
-  }
-
-  //フォームの更新時のハンドラー
-  const handleChange =  (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target
-    switch (name) {
-      case 'title':
-        setTitle(value)
-        break
-      case 'content':
-        setContent(value)
-        break
-      case 'thumbnailUrl':
-        setThumbnailUrl(value)
-        break
-    }
-  }
 
   //編集フォーム
   return (
-    <div>
-      <h1 className="text-xl font-bold mb-8">記事作成</h1>
-      <div className="mb-4">
-        <div className="">タイトル</div>
-        <input id="title" value={title} name="title" type="text" className="w-full border border-gray-300 rounded h-8 p-6" onChange={handleChange}/>
-      </div>
-      <div className="mb-4">
-        <div className="">内容</div>
-        <textarea id="content" value={content} name="content" className="w-full border border-gray-300 rounded h-32 p-6" onChange={handleChange}></textarea>
-      </div>
-      <div className="mb-4">
-        <div className="">サムネイルURL</div>
-        <input id="thumbnailUrl" value={thumbnailUrl} name="thumbnailUrl" type="text" className="w-full border border-gray-300 rounded h-8 p-6" onChange={handleChange}/>
-      </div>
-      <div className="mb-4">
-        <div className="">カテゴリー</div>
-        <CategoriesSelect selectedCategories={categories} setSelectedCategories={setCategories}/>
-        {/* <select id="categories" name="categories" className="w-full border border-gray-300 rounded h-8 p-6">
-            {categories.map(category => {
-                return (
-                    <option key={category.id} value={category.id}>{category.name}</option>
-                )
-            })}
-        </select> */}
-      </div>
-      <div className="flex justify-left">
-        <button type='submit' className="bg-gray-800 text-white font-bold rounded-lg px-6 py-2 mr-4 hover:cursor-pointer" onClick={handleSubmit}>送信</button>
-      </div>
-    </div>
+    <PostForm id={undefined}/>
   )
 }
--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -23,22 +23,21 @@ export default function AdminPosts() {
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           <Link href="/admin/posts/new">新規作成</Link>
         </button>
-    
-      </div>
-      <ul>
+    </div>
+    <ul>
     {
-        posts.map(post => {
-            return(
-              <li key={post.id} className="border-b border-gray-300 p-2 my-2 mb-4 hover:bg-gray-100 cursor-pointer">
-                <Link href={`/admin/posts/${post.id}`}>
-                  <h1 className="text-xl font-bold">{post.title}</h1>
-                  <div className="text-gray-500 text-sm pl-2">{new Date(post.createdAt).toLocaleDateString()}</div>
-                </Link>
-              </li>
-            )
-        })
+      posts.map(post => {
+        return(
+        <li key={post.id} className="border-b border-gray-300 p-2 my-2 mb-4 hover:bg-gray-100 cursor-pointer">
+          <Link href={`/admin/posts/${post.id}`}>
+          <h1 className="text-xl font-bold">{post.title}</h1>
+          <div className="text-gray-500 text-sm pl-2">{new Date(post.createdAt).toLocaleDateString()}</div>
+          </Link>
+        </li>
+        )
+      })
     }
-  </ul> 
-  </div>
-)
+    </ul> 
+    </div>
+  )
 }
